cafc4706b06de77e24c8688f7d21b981c57f7a69
jedrekmancer <andrzej.kaganek@gmail.com>
2023-04-24 15:58:48 +0200

    Started new project RemoveStringSpaces

---
+++ RemoveStringSpaces/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveStringSpaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            //while(true)
+            //{
+            //    if (input.IndexOf(' ') != -1)
+            //        input = input.Remove(input.IndexOf(' '), 1));
+            //    else
+            //        break;
+            //}
+
+            //Console.WriteLine(input);
+
+            List<char> output = new List<char>();
+            foreach (char c in input)
+            {
+            if (c !=' ')
+                    output.Add(c);
+            }
+            Console.WriteLine(output.ToArray());
+            
+
+            Console.ReadLine();
+        }
+    }
+}
